fd5bccb91979812d1976aa6d67f8f4ab9b6e11a1
Emmanuel DK Dolo <emmanueldkdolo1993@gmail.com>
2024-11-06 09:31:17 +0000

    changed the index.css to styles.css

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,7 +4,7 @@ export function TodoList({ todos, toggleTodo, deleteTodo, editTodo }) {
   return (
     <ul className="list">
       {todos.length === 0 ? (
-        <p>No Todos</p>
+        <p className="NoTodo">No Todos</p>
       ) : (
         todos.map((todo) => (
           <TodoItem
